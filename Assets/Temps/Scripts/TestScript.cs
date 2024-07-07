256648c42d0044c418e683f448ebf255ed6a7daa
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-07-07 20:28:40 +0700

    Add some test

---
+++ Assets/Temps/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
