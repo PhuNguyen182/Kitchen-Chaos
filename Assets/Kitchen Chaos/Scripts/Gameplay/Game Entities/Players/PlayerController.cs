f958975cdb733f4549e9d8015b6df3f64e82d27a
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-07-17 23:58:52 +0700

    Update player controller, layer manager

--- Assets/Kitchen Chaos/Scripts/Gameplay/Game Entities/Players/PlayerController.cs
+++ Assets/Kitchen Chaos/Scripts/Gameplay/Game Entities/Players/PlayerController.cs
@@ -7,16 +7,26 @@ namespace KitchenChaos.Scripts.Gameplay.GameEntities.Players
 {
     public class PlayerController : MonoBehaviour
     {
+        [Header("Movement")]
         [SerializeField] private float moveSpeed = 6f;
-        [SerializeField] private Transform playerGraphic;
-        [SerializeField] private Transform groundPoint;
+        [SerializeField] private float wallDistance = 0.15f;
+        [SerializeField] private float wallCheckRadius = 1f;
+        [SerializeField] private float rotateInterpolate = 18f;
+        [SerializeField] private float playerHeight = 2f;
         [SerializeField] private Rigidbody playerBody;
-        [SerializeField] private CapsuleCollider playerCollider;
-        [SerializeField] private LayerMask checkGroundMask;
+
+        [Header("Physics Checking")]
+        [SerializeField] private Transform playerGraphic;
+        [SerializeField] private LayerMask checkObstacleMask;
         [SerializeField] private PlayerAnimation playerAnimation;
 
-        private Vector3 _inputVector;
+        private const float WallHitCheckThreshold = 0.5f;
+
+        private float _hitNormalAngleSine;
+
         private Vector2 _movement;
+        private Vector3 _inputVector;
+        private RaycastHit _kitchenCounterHit;
 
         private void Awake()
         {
@@ -37,11 +47,52 @@ namespace KitchenChaos.Scripts.Gameplay.GameEntities.Players
         private void MovePlayer()
         {
             _inputVector = new(_movement.x, 0, _movement.y);
-            playerBody.MovePosition(playerBody.position + _inputVector * moveSpeed * Time.fixedDeltaTime);
 
             Vector3 playerForward = _inputVector;
-            Vector3 forward = Vector3.Slerp(playerGraphic.forward, playerForward, Time.deltaTime * 18f);
+            Vector3 forward = Vector3.Slerp(playerGraphic.forward, playerForward, Time.deltaTime * rotateInterpolate);
             playerGraphic.forward = forward;
+
+            if (IsMovementInputActive())
+            {
+                if (!CheckPlayerCollision())
+                {
+                    playerBody.MovePosition(playerBody.position + _inputVector * moveSpeed * Time.fixedDeltaTime);
+                }
+
+                else
+                {
+                    Vector3 newInput = Vector3.zero;
+                    if (Mathf.Abs(_hitNormalAngleSine) >= WallHitCheckThreshold && _inputVector.x != 0)
+                        newInput = new(_inputVector.x, 0, 0);
+
+                    else if (Mathf.Abs(_hitNormalAngleSine) <= WallHitCheckThreshold && _inputVector.z != 0)
+                        newInput = new(0, 0, _inputVector.z);
+
+                    playerBody.MovePosition(playerBody.position + newInput.normalized * moveSpeed * Time.fixedDeltaTime);
+                }
+            }
+            
+            playerAnimation.SetWalkingState(IsMovementInputActive());
+        }
+
+        private bool CheckPlayerCollision()
+        {
+            Vector3 heightPoint = transform.position + Vector3.up * playerHeight;
+            bool isCollided = Physics.CapsuleCast(transform.position, heightPoint, wallCheckRadius, _inputVector
+                                                  , out _kitchenCounterHit, wallDistance, checkObstacleMask);
+            float angle = Mathf.Atan2(_kitchenCounterHit.normal.z, _kitchenCounterHit.normal.x);
+            _hitNormalAngleSine = Mathf.Sin(angle);
+            return isCollided;
+        }
+
+        private void HandleKitchenCounter()
+        {
+
+        }
+
+        private bool IsMovementInputActive()
+        {
+            return _inputVector.sqrMagnitude > 0;
         }
     }
 }
